f1d9be3071b3b717f7e6463da656d954e7ee4f98
Damir Salikhov <damir.salihov@inbox.ru>
2022-11-28 00:35:59 +0500

    The functionality for adding the ability to edit an element is implemented

--- index.js
+++ index.js
@@ -6,30 +6,6 @@ const { addNote, getNotes, removeNote, editNote } = require('./notes.controller'
 const port = 3000
 const app = express()
 
-// const server = http.createServer(async (req, res) => {
-//     if (req.method === 'GET') {
-//         const content = await fs.readFile(path.join(basePath, 'index.html'))
-//         res.writeHead(200, {
-//             'Content-Type': 'text/html'
-//         })
-//         res.end(content)
-//     } else if (req.method === 'POST') {
-//         const body = []
-//         res.writeHead(200, {
-//             'Content-Type': 'text/plain; charset=utf-8'
-//         })
-//         req.on('data', data => {
-//             body.push(Buffer.from(data))
-//         })
-//         req.on('end', () => {
-//             const title = body.toString().split('=')[1].replaceAll('+', ' ')
-//             addNote(title)
-//
-//             res.end(`Title = ${title}`)
-//         })
-//     }
-// })
-
 app.set('view engine', 'ejs')
 app.set('views', 'pages')
 
--- public/app.js
+++ public/app.js
@@ -1,14 +1,79 @@
 document.addEventListener('click', (event) => {
     const id = event.target.dataset?.id
 
-    if (event.target.dataset.type === 'remove') {
-        remove(id)
-            .then(() => { event.target.closest('li').remove() })
-    } else if (event.target.dataset.type === 'edit') {
-        const newTitle = prompt('Введите новое название')
-        if (newTitle) {
-            edit(id, {title: newTitle})
-                .then(() => { event.target.closest('li').firstChild.textContent = newTitle })
+    if (id) {
+        const parent = event.target.closest('li')
+        const btnsContainer = event.target.closest('span')
+        const btns = btnsContainer.children
+
+        function changeBtns(type) {
+            switch (type) {
+                case 'main':
+                    btns[0].innerText = 'Обновить'
+                    btns[0].dataset.type = 'update'
+                    btns[1].innerText = '\u{00D7}'
+                    btns[1].dataset.type = 'remove'
+                    breake
+                case 'update':
+                    btns[0].innerText = 'Сохранить'
+                    btns[0].dataset.type = 'edit'
+                    btns[1].innerText = 'Отменить'
+                    btns[1].dataset.type = 'cancel-update'
+                    breake
+            }
+        }
+
+        function toggleBtns(x) {
+            switch (x) {
+                case 'update': {
+                    const title = parent.firstChild.textContent.trim()
+                    const formEditTitle = document.createElement("input")
+                    formEditTitle.type = "text"
+                    formEditTitle.value = title
+                    formEditTitle.dataset.value = title
+                    parent.firstChild.remove()
+                    parent.insertAdjacentElement('afterbegin', formEditTitle)
+                    changeBtns('update')
+                }
+                    break
+                case 'save': {
+                    const title = parent.querySelector('input').value
+                    parent.firstChild.remove()
+                    parent.insertAdjacentHTML('afterbegin', `<span>${title}</span>`)
+                    changeBtns('main')
+                }
+                    break
+                case 'cancel': {
+                    const title = parent.querySelector('input').dataset.value
+                    parent.firstChild.remove()
+                    parent.insertAdjacentHTML('afterbegin', `<span>${title}</span>`)
+                    changeBtns('main')
+                }
+                    break
+            }
+        }
+
+        switch (event.target.dataset.type) {
+            case 'remove':
+                remove(id).then(() => { event.target.closest('li').remove() })
+                break
+            case 'update':
+                toggleBtns('update')
+                break
+            case 'edit':
+                const input = parent.querySelector('input')
+                const newTitle = input.value
+
+                if (newTitle) {
+                    edit(id, {title: newTitle})
+                        .then(() => {
+                            toggleBtns('save')
+                            parent.firstChild.textContent = newTitle })
+                }
+                break
+            case 'cancel-update':
+                toggleBtns('cancel')
+                break
         }
     }
 })
